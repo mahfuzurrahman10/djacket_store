d386ef59623cf5a96db52d4496622fc81052fdd5
Mahfuzur <himel00528@gmail.com>
2022-12-04 23:27:08 +0600

    create a product views and link to details

--- djackets_store/product/views.py
+++ djackets_store/product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -9,4 +10,16 @@ class LatestProductList(APIView):
     def get(self, request, formate = None):
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductDetails(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, category_slug, product_slug,formate = None):
+        product = self.get_object(category_slug,product_slug)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
